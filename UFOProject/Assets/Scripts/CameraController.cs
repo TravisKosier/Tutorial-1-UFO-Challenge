075b94c578cf946cccb61e8fcb085facf5c63e9d
Travis Kosier <tdkosier@comcast.net>
2019-09-21 00:40:59 -0400

    UFO Challenge Completed

---
+++ UFOProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    private int end;
+
+    // Start is called before the first frame update
+ void Start()
+    {
+        offset = transform.position - player.transform.position;
+        end = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+            end = 1;
+        }
+        else if (end != 1) //This is here to prevent unity from throwing hundreds of errors after the game object is destroyed, but it doesn't work, and I'm done trying to get it to work. It's not required for the project.
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ UFOProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    private int count;
+    private int lives;
+    public Text countText;
+    public Text lifeText;
+    public Text winText;
+    private int lvl;
+    private bool lost;
+    private bool won;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        lost = false;
+        won = false;
+        lvl = 1;
+        SetCountText();
+        SetLifeText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement*speed);
+        /*if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }*/
+
+        if (count == 12 && lvl == 1)
+        {
+            lvl = 2;
+            transform.position = new Vector2(50.0f, 50.0f); 
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp") && lost == false)
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy") && won == false) {
+            other.gameObject.SetActive(false);
+            lives--;
+            SetLifeText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20 && lost != true) { //20 now
+            won = true; //Prevents losing after winning
+            winText.text = "You win! Game created by Travis Kosier.";
+        }
+        
+    }
+
+    void SetLifeText()
+    {
+        lifeText.text = "Lives: " + lives.ToString();
+        if (lives == 0) //If player runs out of lives
+        {
+            lost = true; //Prevents winning after losing
+            winText.text = "You lose! Game created by Travis Kosier.";
+            Destroy(gameObject);
+
+        }
+    }
+
+}
